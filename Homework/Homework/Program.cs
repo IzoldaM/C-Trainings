2e91364945b32469aaade51267756139dbc4e72c
Izolda_M <motiejauskyte.izolda@gmail.com>
2021-11-30 15:39:12 +0200

    3rd commit - branch commit test

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -4,9 +4,31 @@ namespace Homework
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello Lessons!");
+            /* 3 - new branch task*/
+            Console.WriteLine(IsPrimal(3));
         }
-    }
+        static bool IsPrimal(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 2; i < number; i++)
+                {
+                    if (number%i == 0)
+                    {
+                        return false;
+                    }
+
+                }
+                return true;
+            }
+        }
+       
+
+    }   
 }
