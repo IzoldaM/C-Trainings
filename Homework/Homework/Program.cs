c017497b0d080dd6dab22f01b67230fc9f69cbef
Izolda_M <motiejauskyte.izolda@gmail.com>
2021-11-30 15:06:56 +0200

    1st task commit
    Test commit - first method

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -4,9 +4,35 @@ namespace Homework
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello Lessons!");
+            /*1 - Git commit test task*/
+            Console.WriteLine(Max_function(10, 5, 3));
+
+        }
+        static int Max_function (int a, int b, int c)
+        {
+            int largest_num;
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    largest_num = a;
+                }
+                else
+                {
+                    largest_num = c;
+                }
+            }
+            else if (b > c)
+            {
+                largest_num = b;
+            }
+            else
+            {
+                largest_num = c;
+            }
+            return largest_num;
         }
     }
 }
